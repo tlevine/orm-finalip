5129f11f41c02e4cd1746d67d7a2f5ff215da603
Thomas Levine <occurrence@thomaslevine.com>
2013-06-15 20:26:55 -0700

    plot a graph

---
+++ finalip.r
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+library(sqldf)
+library(plyr)
+library(ggplot2)
+library(reshape2)
+
+grey <- '#333333'
+
+if (!('ip' %in% ls())) {
+  ip <- sqldf('SELECT * FROM "finalip";', dbname = '/tmp/finalip.db')
+}
+
+n.decisions <- ddply(ip, 'District', function(df) {c(Decisions=nrow(df))})
+n.decisions <- n.decisions[order(n.decisions$Decisions, decreasing = T),]
+
+png('finalip.png', width = 1600, height = 900, res = 150)
+par(las = 2, mar = c(6, 4, 4, 0),
+    col = grey, fg = grey, col.main = grey,
+    col.axis = grey, col.lab = grey)
+barplot(
+    n.decisions$Decisions, names.arg = n.decisions$District,
+    main = 'ORM Final Individual Permit Decisions by District',
+    xlab = '',
+    ylab = 'Count since 2009',
+    border = NA
+)
+par(las = 0)
+mtext('Nota bene: This dataset is not complete.', side = 3, padj = 1)
+dev.off()
